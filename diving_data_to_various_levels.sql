e35ac826dfd0634b444bc466c81fdbae97e57c2b
paullaster <paullasterokoth98@gmail.com>
2022-09-06 17:12:37 +0300

    percentile

--- diving_data_to_various_levels.sql
+++ diving_data_to_various_levels.sql
@@ -31,3 +31,20 @@ occurred_at,
 gloss_qty,
 NTILE(2) OVER(PARTITION BY account_id ORDER BY standard_qty) gloss_half
 FROM orders
+
+
+/*
+Use the NTILE functionality to 
+divide the orders for each account into 100 levels in 
+terms of the amount of total_amt_usd for their orders. 
+Your resulting table should have the account_id, 
+the occurred_at time for each order, the total amount 
+of total_amt_usd paper purchased, and one of 100 
+levels in a total_percentile column.
+*/
+SELECT 
+account_id,
+occurred_at,
+total_amt_usd,
+NTILE(100) OVER(PARTITION BY account_id ORDER BY total_amt_usd) total_percentile
+FROM orders
