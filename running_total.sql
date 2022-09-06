653280af01b78a682d8f2209cafb665ad0b6fc38
paullaster <paullasterokoth98@gmail.com>
2022-09-06 10:59:00 +0300

    Solution implementing year partition

--- running_total.sql
+++ running_total.sql
@@ -1,11 +1,17 @@
 /*
 create a running total of standard_amt_usd
- (in the orders table) over order time 
- with no date truncation. Your final table
- should have two columns: one with the 
- amount being added for each new row, 
- and a second with the running total.
+ (in the orders table) over order,
+  date truncate occurred_at by year
+   and partition by that same year-truncated
+    occurred_at variable. 
+    Your final table should have three columns:
+     One with the amount
+  being added for each row, 
+  one for the truncated date, 
+  and a final column with the running total within each year.
 */
-SELECT standard_amt_usd,
-SUM(standard_amt_usd) OVER(ORDER BY occurred_at) running_total
+SELECT DATE_TRUNC('year', occurred_at) occurred_at,
+standard_amt_usd,
+SUM(standard_amt_usd) 
+OVER(PARTITION BY  DATE_TRUNC('year', occurred_at) ORDER BY occurred_at) running_total
 FROM orders
