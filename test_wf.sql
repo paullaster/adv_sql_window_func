68391215d5ec5089ca5d9bab5fbb61c35bd1b603
paullaster <paullasterokoth98@gmail.com>
2022-09-07 09:28:53 +0300

    CASTING

--- test_wf.sql
+++ test_wf.sql
@@ -9,15 +9,20 @@ total number of posts up until a given date.
 The resulting set should be ordered 
 chronologically by date.
 */
+/*
+CTE transform data and count the post per date
+*/
 WITH t1 AS (SELECT
-DATE(DATE_TRUNC('day',created_at)) AS date,
-COUNT(DATE(DATE_TRUNC('day',created_at))) AS count
+  DATE(DATE_TRUNC('day',created_at)) AS date,
+  CAST(COUNT(DATE(DATE_TRUNC('day',created_at))) AS INTEGER) AS count
 FROM posts
 GROUP BY 1)
-      
+/*
+SELECTING FROM CTE
+*/      
 SELECT
-date,
-count,
-SUM(count) 
-OVER(PARTITION BY  DATE_TRUNC('month', date) ORDER BY date)total
+  date,
+  count,
+  SUM(count) 
+  OVER(ORDER BY date)total
 FROM t1
